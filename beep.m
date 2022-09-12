f222992eff8798d2389486ba20bb8bc0df6ddbbf
NduvhoEdward <Nduvho.Ramashia@gmail.com>
2022-09-12 19:33:38 +0200

    Appropriately padded the end of the tone with 2 seconds of solid sounds, and saved the appropriate array part into a new array, leaving out the silence part of the array

--- beep.m
+++ beep.m
@@ -5,34 +5,32 @@ f = 523.25*2;
 t = (0:T:60-T);
 A_t = 0.01*t + 0.5;
 y_t = A_t.*sin(2*pi*f*t);
-disp("qqqqqqqqqqqqq 1")
 
 sig_length = length(t);
 beep_sequence = zeros(1,sig_length);
 beep_period = fs/2; 
 beep_length = ceil(fs*0.03);
-disp("qqqqqqqqqqqqq 2")
 
 beep_period_base = beep_period;
 period_dec_rate = ceil(beep_period_base*exp(-t/20));
-disp("qqqqqqqqqqqqq 3")
 
 beeps = []; 
+%beeps(end+1:end+10/T) = 0:beep_period_base:10/T;
 for i=1:beep_period_base:sig_length
-		beeps(end+1:end+beep_period_base) = beep_period;
-		if period_dec_rate(i)>200
-			beep_period = beep_period + period_dec_rate(i);
-		end
+	beeps(end+1:end+beep_period_base) = beep_period;
+	if period_dec_rate(i)>200
+		beep_period = beep_period + period_dec_rate(i);
+	end
 end
-disp("qqqqqqqqqqqqq 4")
 
 for i = beeps
 	beep_sequence(i:i+beep_length) = 1; 
 end
-disp("qqqqqqqqqqqqq 5")
+beep_sequence(19/T:22/T) = 1;
 
 y_t = A_t.*sin(2*pi*f*t).*beep_sequence;
-disp("qqqqqqqqqqqqq 6")
 
-%sound(y_t)
+y_beep = y_t(1:22/T);
+
+%sound(y_beep)
 plot(t, y_t);
