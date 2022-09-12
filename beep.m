c46947b36d71c82e947fe2ef1804a7024c1ee60b
NduvhoEdward <Nduvho.Ramashia@gmail.com>
2022-09-12 20:09:08 +0200

    Added the variance in frequency with time - Made the frequency increase with time for more'egency' effects

--- beep.m
+++ beep.m
@@ -1,10 +1,15 @@
 clear all;
 fs = 8192;
 T = 1/fs;
-f = 523.25*2;
-t = (0:T:60-T);
-A_t = 0.01*t + 0.5;
-y_t = A_t.*sin(2*pi*f*t);
+duration = 60;
+t = (0:T:duration-T);
+f_init = 523.25;
+f_end = f_init*2*2;
+total_samples = fs*duration;
+
+f_step = (f_end - f_init)/total_samples;
+f = f_init:f_step:f_end-f_step;
+A_t = 0.02*t + 0.5;
 
 sig_length = length(t);
 beep_sequence = zeros(1,sig_length);
@@ -15,7 +20,6 @@ beep_period_base = beep_period;
 period_dec_rate = ceil(beep_period_base*exp(-t/20));
 
 beeps = []; 
-%beeps(end+1:end+10/T) = 0:beep_period_base:10/T;
 for i=1:beep_period_base:sig_length
 	beeps(end+1:end+beep_period_base) = beep_period;
 	if period_dec_rate(i)>200
@@ -26,11 +30,11 @@ end
 for i = beeps
 	beep_sequence(i:i+beep_length) = 1; 
 end
-beep_sequence(19/T:22/T) = 1;
+beep_sequence(19/T:21.5/T) = 1;
 
-y_t = A_t.*sin(2*pi*f*t).*beep_sequence;
+y_t = A_t.*sin(2*pi*f.*t).*beep_sequence;
 
-y_beep = y_t(1:22/T);
+y_beep = y_t(1:21.5/T);
 
 %sound(y_beep)
 plot(t, y_t);
