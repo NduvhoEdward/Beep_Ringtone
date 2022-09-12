1aa3ed591b34d3864173dc38d913f93fcf77920c
NduvhoEdward <Nduvho.Ramashia@gmail.com>
2022-09-12 20:29:31 +0200

    Cleaned the base code for the beep and added the exporting-sound code

--- beep.m
+++ beep.m
@@ -1,11 +1,12 @@
 clear all;
-fs = 8192;
-T = 1/fs;
+
+Fs = 8192;
+T = 1/Fs;
 duration = 60;
 t = (0:T:duration-T);
 f_init = 523.25;
 f_end = f_init*2*2;
-total_samples = fs*duration;
+total_samples = Fs*duration;
 
 f_step = (f_end - f_init)/total_samples;
 f = f_init:f_step:f_end-f_step;
@@ -13,8 +14,8 @@ A_t = 0.02*t + 0.5;
 
 sig_length = length(t);
 beep_sequence = zeros(1,sig_length);
-beep_period = fs/2; 
-beep_length = ceil(fs*0.03);
+beep_period = Fs/2; 
+beep_length = ceil(Fs*0.03);
 
 beep_period_base = beep_period;
 period_dec_rate = ceil(beep_period_base*exp(-t/20));
@@ -37,4 +38,7 @@ y_t = A_t.*sin(2*pi*f.*t).*beep_sequence;
 y_beep = y_t(1:21.5/T);
 
 %sound(y_beep)
-plot(t, y_t);
+%plot(t, y_t);
+
+filename = 'Beep.wav';
+audiowrite(filename,y_beep,Fs);
